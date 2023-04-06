ecbda0fe60acf15ce24d0a02a15fc1f7f5a6eb60
Murilothebr <murilothebr@hotmail.com>
2023-04-06 09:06:03 -0300

    feat: json server and fetch

--- src/Scripts/Modal.js
+++ src/Scripts/Modal.js
@@ -1,10 +1,23 @@
-function openModal(restaurante) {
-    var modal = document.getElementById(`modal-${restaurante}`);
-    modal.classList.add("active");
+function popularRestaurantes(json) {
+  var restaurantes = json.restaurantes;
+  var ul = document.getElementById("restaurantes-list");
+
+  for (var i = 0; i < restaurantes.length; i++) {
+    var div = document.createElement("div");
+    div.innerText = restaurantes[i].name;
+    ul.appendChild(div);
   }
   
-  function closeModal(restaurante) {
-    var modal = document.getElementById(`modal-${restaurante}`);
-    modal.classList.remove("active");
-  }
-  
+}
+
+fetch('/db.json')
+  .then(response => response.json())
+  .then(data => {
+    // Chamando a função para popular a lista de usuários com o objeto JSON
+    popularRestaurantes(data);
+  })
+  .catch(error => console.error(error));
+
+function alerta(){
+  window.alert('alerta');
+}
